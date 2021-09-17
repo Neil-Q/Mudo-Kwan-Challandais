2aa197acaab6ec908c16de4e075096f511af77c5
Neil-Q <neil.quechon@gmail.com>
2021-09-17 15:18:31 +0200

    Ajout de la bannière des cours d'essais gratuits

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -11,7 +11,16 @@
         </div>
     </section>
     <Practices />
-    <div id="free_try_dummy"></div>
+    <div id="free_try">
+        <div id="free_try_picture">
+            <img src="../assets/club_pictures/2021-2022_new_season_group.jpg" alt="Photo de groupe du premier cours de la saison 2021-2022" />
+        </div>
+        <div id="free_try_text">
+            <p>2 cours d'essai gratuits</p>
+            <p>Viens découvrir le Taekwondo ou le Hapkimudo durant deux séances et trouve la voie martiale  qui te correspond</p>
+            <p>Tu peux venir avec un ou plusieurs de tes amis ou des membres de ta famille !</p>
+        </div>
+    </div>
     <div id="horraires_dummy"></div>
     <div id="footer_dummy"></div>
 </template>
@@ -29,7 +38,10 @@ export default {
         NavBar,
         Practices
     },
-    methods: {
+    data() {
+        return {
+            clubPicture: require('@/assets/club_pictures/2021-2022_new_season_group.jpg')
+        }
     }
 }
 </script>
@@ -51,10 +63,6 @@ body{
     }
 }
 //___________________________________________________________
-#free_try_dummy {
-    height: 35rem;
-    background-color: var(--blue-dark-color);
-}
 #horraires_dummy {
     height: 60rem;
 }
@@ -101,4 +109,52 @@ body{
     }
 }
 
+#free_try {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background-color: var(--blue-dark-color);
+    padding: 2rem 10% 2rem 10%;
+
+    #free_try_picture {
+        width: 45%;
+
+        img {
+            max-width: 100%;
+        }
+    }
+
+    #free_try_text{
+        color: white;
+        font-size: clamp(0.8rem, 1.2vw, 1.2rem);
+        width: 45%;
+
+        :first-child{
+            text-transform: uppercase;
+            text-decoration: underline;
+            font-size: clamp(1rem, 1.5vw, 1.5rem);
+            font-weight: bold;     
+        }
+    }
+}
+
+@media (max-width: 40em) {
+    #free_try {
+        flex-direction: column;
+        padding: 2rem 0 2rem 0;
+
+        #free_try_picture {
+            width: 85%;
+        }
+
+        #free_try_text {
+            width: 85%;
+            font-size: 1rem;
+
+            :first-child {
+                font-size: 1.2rem;
+            }
+        }
+    }
+}
 </style>
