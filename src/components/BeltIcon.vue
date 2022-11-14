14623b91a92e37989efc4c7884452fd9344dff90
Neil-Q <neil.quechon@gmail.com>
2022-11-14 22:10:46 +0100

    Création du composant BeltIcon

---
+++ src/components/BeltIcon.vue
@@ -0,0 +1,189 @@
+<template>
+    <div class="belt">
+        <div class="stripe stripe_1"></div>
+        <div class="stripe stripe_2"></div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "beltIcon",
+        props: {
+            discipline: {
+                default: "taekwondo",
+                validator(value) {
+                    return ["taekwondo", "taekwondo_minus_twelve", "hapkimudo"].includes(value)
+                }
+            },
+            keup: {
+                type: Number,
+                default: 0
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    .belt {
+        box-sizing: border-box;
+        width: 100%;
+        max-width: 100%;
+        height: 100%;
+        max-height: 100%;
+        display: grid;
+        grid-template-rows: 1fr;
+        grid-template-areas: "a b c d e";
+        border: 2px solid var(--dark-color);
+        border-radius: 3px;
+
+        &.wide {
+            aspect-ratio: 5/1;
+            grid-template-columns: 10fr 1fr 1fr 1fr 2fr;
+        }
+        &.medium {
+            aspect-ratio: 4/1;
+            grid-template-columns: 7fr 1fr 1fr 1fr 2fr;
+        }
+        &.small {
+            aspect-ratio: 3/1;
+            grid-template-columns: 5fr 1fr 1fr 1fr 1fr;
+        }
+
+        .stripe {
+            width: 100%;
+            height: 100%;
+            background-color: var(--stripes-color);
+            outline: 5px solid var(--stripes-color);
+        }
+        .stripe_1 {
+            grid-area: b;
+        }
+        .stripe_2 {
+            grid-area: d;
+        }
+    }
+
+    .taekwondo {
+        // Belt color
+        &.keup_10 {
+            background-color: var(--belt-white);
+        }
+        &.keup_9, &.keup_8, &.keup_7 {
+            background-color: var(--belt-yellow);
+            --stripes-color: var(--belt-blue);
+        }
+        &.keup_6, &.keup_5, &.keup_4 {
+            background-color: var(--belt-blue);
+            --stripes-color: var(--belt-red);
+        }
+        &.keup_3, &.keup_2, &.keup_1 {
+            background-color: var(--belt-red);
+            --stripes-color: var(--belt-black);
+        }
+
+        // Stripes 
+
+        &.keup_10, &.keup_9, &.keup_6, &.keup_3 {
+            .stripe_1, .stripe_2 {
+                display: none;
+            }
+        }
+
+        &.keup_8, &.keup_5, &.keup_2 {
+            .stripe_1 {
+                display: none;
+            }
+        }
+    }
+
+    .taekwondo_children {
+        // Belt color
+        &.keup_15 {
+            background-color: var(--belt-white);
+        }
+        &.keup_14, &.keup_13 {
+            background-color: var(--belt-yellow);
+            --stripes-color: var(--belt-orange);
+        }
+        &.keup_12, &.keup_11 {
+            background-color: var(--belt-orange);
+            --stripes-color: var(--belt-green);
+        }
+        &.keup_10, &.keup_9 {
+            background-color: var(--belt-green);
+            --stripes-color: var(--belt-purple);
+        }
+        &.keup_8, &.keup_7 {
+            background-color: var(--belt-purple);
+            --stripes-color: var(--belt-blue);
+        }
+        &.keup_6, &.keup_5, &.keup_4 {
+            background-color: var(--belt-blue);
+            --stripes-color: var(--belt-red);
+        }
+        &.keup_3, &.keup_2, &.keup_1 {
+            background-color: var(--belt-red);
+            --stripes-color: var(--belt-black);
+        }
+
+        // Stripes 
+
+        &.keup_15, &.keup_14, &.keup_12, &.keup_10, &.keup_8, &.keup_6, &.keup_3 {
+            .stripe_1, .stripe_2 {
+                display: none;
+            }
+        }
+
+        &.keup_13, &.keup_11, &.keup_9, &.keup_7, &.keup_5, &.keup_2 {
+            .stripe_1 {
+                display: none;
+            }
+        }
+    }
+
+    .hapkimudo {
+        // Belt color
+        &.keup_10 {
+            background-color: var(--belt-white);
+        }
+        &.keup_9, &.keup_8 {
+            background-color: var(--belt-yellow);
+            --stripes-color: var(--belt-green);
+        }
+        &.keup_7, &.keup_6 {
+            background-color: var(--belt-green);
+            --stripes-color: var(--belt-blue);
+        }
+        &.keup_5, &.keup_4 {
+            background-color: var(--belt-blue);
+            --stripes-color: var(--belt-red);
+        }
+        &.keup_3, &.keup_2, &.keup_1 {
+            background-color: var(--belt-red);
+            --stripes-color: var(--belt-black);
+        }
+
+        // Stripes 
+
+        &.keup_10, &.keup_9, &.keup_7, &.keup_5, &.keup_3 {
+            .stripe_1, .stripe_2 {
+                display: none;
+            }
+        }
+
+        &.keup_8, &.keup_6, &.keup_4, &.keup_2 {
+            .stripe_1 {
+                display: none;
+            }
+        }
+    }
+
+    .dan_1 {
+        background-color: var(--belt-black);
+        --stripes-color: var(--belt-yellow);
+
+        .stripe_1 {
+            display: none;
+        }
+    }
+</style>
---
+++ src/components/TaekwondoKeupPanel.vue
@@ -0,0 +1,160 @@
+<template>
+    <div id="rank_selector_panel">
+        <input id="show_all_radio_selector" class="grade_global_selector" type="radio" name="grade_global_selector" checked>
+        <label for="show_all_radio_selector" @click="updateShowAll(true)">
+            <div class="illustration"></div>
+            <div class="caption">Voire tout</div>
+        </label>
+
+        <div class="separator"></div>
+
+        <div id="age_selectors">
+            <input id="minus_twelve_radio_selector" class="grade_global_selector" type="radio" name="grade_global_selector">
+            <label for="minus_twelve_radio_selector" @click="updateShowAll(false); updateIsAdult(false)">
+                <div class="illustration"></div>
+                <div class="caption">-12 ans</div>
+            </label>
+            <input id="over_twelve_radio_selector" class="grade_global_selector" type="radio" name="grade_global_selector">
+            <label for="over_twelve_radio_selector" @click="updateShowAll(false); updateIsAdult(true)">
+                <div class="illustration"></div>
+                <div class="caption">+12 ans</div>
+            </label>
+        </div>
+
+        <BeltRankDropdown id="keup_selector" :discipline="isAdult ? 'taekwondo' : 'taekwondo_children'"/>
+    </div>
+</template>
+
+<script>
+    // import BeltIcon             from "@/components/BeltIcon.vue";
+    import BeltRankDropdown     from "@/components/BeltRankDropdown.vue";
+
+    export default {
+        name: "TaekwondoKeupPanel",
+        components: {
+            // BeltIcon,
+            BeltRankDropdown
+        },
+        data() {
+            return {
+                showAll: true,
+                isAdult: true,
+                keup: 0
+            }
+        },
+        methods: {
+            // setSelectors(showAll, isAdult, keup) {
+                
+            // },
+            updateShowAll(boolean) {
+                this.showAll = boolean;
+                // alert("showAll: " + this.showAll);
+            },
+            updateIsAdult(boolean) {
+                this.isAdult = boolean;
+                // alert("isAdulte: " + this.isAdulte);
+            },
+            updateKeup(keup) {
+                this.keup = keup;
+                // alert("keup: " + this.keup);
+            }
+        }
+    }
+</script>
+
+<style lang="scss">
+    input[type="radio"] {
+        display: none;
+    }
+
+    #rank_selector_panel{
+        --global-padding: clamp(1rem, 7vw, 2rem);
+
+        box-sizing: border-box;
+        display: flex;
+        flex-direction: column;
+        gap: var(--global-padding);
+        padding: var(--global-padding);
+        border-radius: 30px;
+        max-width: 25rem;
+        min-width: 15rem;
+        background-color: var(--dark-color);
+        margin: auto;
+
+        label, #keup_selector {
+            box-sizing: border-box;
+            border-radius: 10px;
+        }
+
+        .separator {
+            border: solid 1px var(--white-color);
+        }
+    }
+    .grade_global_selector {
+        + label {
+            display: block;
+            background-color: silver;
+            cursor: pointer;
+        }
+
+        &:checked + label {
+            background-color: var(--white-color);
+            color: var(--blue-medium-color);
+            outline: 3px solid var(--blue-light-color);
+            outline-offset: 2px;
+        }
+    }
+    #show_all_radio_selector + label {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: space-between;
+        margin: auto;
+        width: 100%;
+        aspect-ratio: 4/1;
+        padding: 0.5rem;
+
+        .illustration {
+            flex: 1;
+            width: 80%;
+            background-color: grey;
+        }
+    }
+    #age_selectors {
+        display: flex;
+        justify-content: center;
+        gap: var(--global-padding);
+
+        label {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: space-between;
+            flex: 1;
+            padding: 1rem;
+            aspect-ratio: 1/1;
+        }
+    }
+    #keup_selector {
+        aspect-ratio: 5/1;
+    }
+
+    // @media (min-width: 60rem) {
+    //     #rank_selector_panel {
+    //         flex-direction: row;
+    //         min-width: 55rem;
+    //         max-height: 10rem;
+
+    //     }
+
+    //     #show_all_radio_selector + label {
+    //         width: auto;
+    //         aspect-ratio: 1 / 1;
+    //     }
+
+    //     #keup_selector {
+    //         flex: 2;
+    //     }
+    // }
+
+</style>
