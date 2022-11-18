a678b9811dde80578d701bc0b9ca201326deed44
Neil-Q <neil.quechon@gmail.com>
2022-11-18 16:50:50 +0100

    Ajout des plans et descriptifs

--- src/components/belt_exams/PoomseTable.vue
+++ src/components/belt_exams/PoomseTable.vue
@@ -19,7 +19,7 @@
         </ul>
 
         <div id="poomse_presentation">
-            <h3>{{currentPoomseName}}</h3>
+            <h3>{{currentPoomseName}} <span>{{currentPoomseTrigram}}</span></h3>
             <h4>{{currentPoomseMeaning}}</h4>
 
             <iframe
@@ -31,21 +31,54 @@
             </iframe>
 
             <div id="poomse_img_and_pdf">
-                <div id="poomse_plan_image">
-                    IMG
-                </div>
+                <button
+                    id="poomse_plan_image"
+                    @click="$refs.poomse_img_popup.open()"
+                >
+                    <svg viewBox="0 0 145.521 132.292" xmlns="http://www.w3.org/2000/svg">
+                        <g transform="translate(-31.806 -7.52319)">
+                            <rect class="horizontal" x="31.806" y="7.52319" width="145.521" height="26.4583"/>
+                            <rect class="vertical" x="84.7226" y="7.52319" width="39.6875" height="132.292"/>
+                            <rect class="horizontal" x="31.806" y="60.4399" width="145.521" height="26.4583"/>
+                            <rect class="horizontal" x="31.806" y="113.357" width="145.521" height="26.4583"/>
+                            <g class="dots">
+                                <circle cx="78.2252" cy="27.8601" r="4.00182"/><circle cx="130.793" cy="27.8601" r="4.00182"/><circle cx="90.9713" cy="60.4399" r="4.00182"/><circle cx="90.9713" cy="86.8982" r="4.00182"/><circle cx="90.9713" cy="73.669" r="4.00182"/><circle cx="90.9713" cy="33.9815" r="4.00182"/><circle cx="90.9713" cy="47.2107" r="4.00182"/><circle cx="90.9713" cy="113.357" r="4.00182"/><circle cx="90.9713" cy="100.127" r="4.00182"/><circle cx="116.678" cy="106.714" r="4.00182"/><circle cx="116.678" cy="93.4853" r="4.00182"/><circle cx="78.2252" cy="132.996" r="4.00182"/><circle cx="130.793" cy="119.776" r="4.00182"/><circle cx="40.341" cy="80.0086" r="4.00182"/><circle cx="78.2252" cy="80.0086" r="4.00182"/><circle cx="59.2831" cy="80.0086" r="4.00182"/><circle cx="130.793" cy="67.2075" r="4.00182"/><circle cx="168.678" cy="67.2075" r="4.00182"/><circle cx="149.736" cy="67.2075" r="4.00182"/><circle cx="59.2831" cy="132.996" r="4.00182"/><circle cx="149.736" cy="119.776" r="4.00182"/>
+                            </g>
+                        </g>
+                    </svg>
+                    <span>PLAN</span>
+                </button>
+                <PopUp id="poomse_img_popup" ref="poomse_img_popup" shape="auto_max">
+                    <template v-slot:content>
+                        <div class="image_wrapper">
+                            <img :src="currentPoomseImage" alt="Plan du poomse en image">
+                        </div>
+                    </template>
+                </PopUp>
+
                 <div class="separator"></div>
-                <div id="poomse_plan_pdf">
-                    PDF
-                </div>
+
+                <a id="poomse_plan_pdf" :href="currentPoomsePDF" target="_blank">
+                    <svg fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5" />
+                    </svg>
+                    <span>
+                        DESCRIPTIF
+                    </span>
+                </a>
             </div>
         </div>
     </div>
 </template>
 
 <script>
+    import PopUp    from "@/components/PopUp.vue";
+
     export default {
         name: "PoomseTable",
+        components: {
+            PopUp
+        },
         props: {
             poomse: {
                 type: Number,
@@ -59,11 +92,20 @@
             currentPoomseName() {
                 return this.poomseDatas[this.currentPoomse - 1].name;
             },
+            currentPoomseTrigram() {
+                return this.poomseDatas[this.currentPoomse - 1].trigram;
+            },
             currentPoomseMeaning() {
                 return this.poomseDatas[this.currentPoomse - 1].meaning;
             },
             currentPoomseVideoAdress() {
                 return this.rvereAdress + this.poomseDatas[this.currentPoomse - 1].youtubeId + '&start=' + this.poomseDatas[this.currentPoomse - 1].videoStart
+            },
+            currentPoomseImage() {
+                return this.poomseDatas[this.currentPoomse - 1].image;
+            },
+            currentPoomsePDF() {
+                return this.poomseDatas[this.currentPoomse - 1].PDF;
             }
         },
         data() {
@@ -73,82 +115,82 @@
                     {
                         number: 1,
                         name: "Il Jang",
-                        trigram: "",
+                        trigram: "☰",
                         meaning: "Le ciel",
                         youtubeId: "WhkjRruCBTo",
                         videoStart: 82,
-                        image: "@/assets/illustrations/poomse/poomse_1.jpg",
-                        // planPDF: 
+                        image: require("@/assets/illustrations/poomse/poomse_1.jpg"),
+                        PDF: "https://www.fftda.fr/files/file/Grades/Vid%C3%A9o/Taegueug%20Il%20Jang.pdf",
                     },
                     {
                         number: 2,
                         name: "Yi jang",
-                        trigram: "",
+                        trigram: "☱",
                         meaning: "Le lac",
                         youtubeId: "tGlrUplKHh8",
                         videoStart: 66,
-                        image: "@/assets/illustrations/poomse/poomse_2.jpg",
-                        // planPDF: 
+                        image: require("@/assets/illustrations/poomse/poomse_2.jpg"),
+                        PDF: "https://www.fftda.fr/files/file/Grades/Vid%C3%A9o/Taegueug%20Y%20Jang.pdf",
                     },
                     {
                         number: 2,
                         name: "Sam Jang",
-                        trigram: "",
+                        trigram: "☲",
                         meaning: "Le feu",
                         youtubeId: "ksSqKt0UkWo",
                         videoStart: 70,
-                        image: "@/assets/illustrations/poomse/poomse_3.jpg",
-                        // planPDF: 
+                        image: require("@/assets/illustrations/poomse/poomse_3.jpg"),
+                        PDF: "https://www.fftda.fr/files/file/Grades/Vid%C3%A9o/Taegueug%20Sam%20Jang.pdf",
                     },
                     {
                         number: 4,
                         name: "Sa Jang",
-                        trigram: "",
+                        trigram: "☳",
                         meaning: "La foudre",
                         youtubeId: "Lt917gacJho",
                         videoStart: 90,
-                        image: "@/assets/illustrations/poomse/poomse_4.jpg",
-                        // planPDF: 
+                        image: require("@/assets/illustrations/poomse/poomse_4.jpg"),
+                        PDF: "https://www.fftda.fr/files/file/Grades/Vid%C3%A9o/DS_TAEGUEUG_SA_JANG.pdf",
                     },
                     {
                         number: 5,
                         name: "Oh Jang",
-                        trigram: "",
+                        trigram: "☴",
                         meaning: "Le vent",
                         youtubeId: "VdqNEAHWCBM",
                         videoStart: 73,
-                        image: "@/assets/illustrations/poomse/poomse_5.jpg",
-                        // planPDF: 
+                        image: require("@/assets/illustrations/poomse/poomse_5.jpg"),
+                        PDF: "https://www.fftda.fr/files/file/Grades/Vid%C3%A9o/DS_TAEGUEUG_OH_JANG.pdf",
                     },
                     {
                         number: 6,
                         name: "Yuk Jang",
-                        trigram: "",
+                        trigram: "☵",
                         meaning: "L'eau",
                         youtubeId: "jcBwWo4wN7c",
                         videoStart: 55,
-                        image: "@/assets/illustrations/poomse/poomse_6.jpg",
-                        // planPDF: 
+                        image: require("@/assets/illustrations/poomse/poomse_6.jpg"),
+                        PDF: "https://www.fftda.fr/files/file/Grades/Vid%C3%A9o/Taegueug%20Yuk%20Jang.pdf",
                     },
                     {
                         number: 7,
                         name: "Tchil Jang",
-                        trigram: "",
+                        trigram: "☶",
                         meaning: "La montagne",
                         youtubeId: "6FUM1p6qqhQ",
                         videoStart: 61,
-                        image: "@/assets/illustrations/poomse/poomse_7.jpg",
-                        // planPDF: 
+                        image: require("@/assets/illustrations/poomse/poomse_7.jpg"),
+                        PDF: "https://www.fftda.fr/files/file/Grades/Vid%C3%A9o/DS_TAEGUEUG_TCHIL_JANG.pdf",
                     },
                     {
                         number: 8,
                         name: "Pal Jang",
-                        trigram: "",
+                        trigram: "☷",
                         meaning: "La terre",
                         youtubeId: "Gr_Je2ZkgkI",
                         videoStart: 68,
-                        image: "@/assets/illustrations/poomse/poomse_8.jpg",
-                        // planPDF: 
+                        image: require("@/assets/illustrations/poomse/poomse_8.jpg"),
+                        PDF: "https://www.fftda.fr/files/file/Grades/Vid%C3%A9o/Taegueug_Pal_Jang_Officiel.pdf",
                     },
                 ],
                 rvereAdress: "https://tube.rvere.com/embed?v=",
@@ -178,7 +220,7 @@
     }
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 
     #poomse_list {
         display: flex;
@@ -238,6 +280,9 @@
         h3 {
             margin-top: clamp(2rem, 5vw, 5rem);
             margin-bottom: 0;
+            span {
+                font-size: $font-sz-xxxl;
+            }
         }
         h4 {
             margin-top: 0;
@@ -267,14 +312,56 @@
 
     #poomse_plan_image, #poomse_plan_pdf {
         @extend %flex-center;
-
+        box-sizing: border-box;
+        flex-direction: column;
+        gap: 1rem;
         height: clamp(4rem, 12vw, 12rem);
+        padding: 2rem;
         aspect-ratio: 1/ 1;
         background-color: $white;
         border: 1px solid $white-3;
-        color: $blue-dark;
+        color: $dark-2;
         font-weight: bold;
-        // box-shadow: $shadow-2;
+        font-size: $font-sz-large;
+        cursor: pointer;
+
+        filter: grayscale(1) opacity(0.7);
+        transition: 0.5s;
+
+        svg {
+            flex: 1;
+        }
+
+        &:hover {
+           filter: grayscale(0.5) opacity(1);
+        }
+    }
+    #poomse_plan_image svg {
+        .horizontal {
+            fill: $blue-light;
+        }
+        .vertical {
+            fill: $white-35;
+        }
+        .dots {
+            fill: $dark-2;
+        }
+    }
+    #poomse_plan_pdf svg {
+        color: $blue;
     }
 
+    #poomse_img_popup {
+        .image_wrapper {
+            @extend %flex-center;
+            box-sizing: border-box;
+            height: 100%;
+            max-height: 100%;
+
+            img {
+                max-height: 100%;
+                max-width: 100%;
+            }
+        }
+    }
 </style>
